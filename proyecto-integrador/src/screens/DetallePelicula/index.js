240264c2a746f0f875dfeac9e9756f5f4dc7040c
CatalinaMaria <cdearzubiagamenghi@udesa.edu.ar>
2023-09-06 16:36:12 -0300

    css final de detalle

--- proyecto-integrador/src/screens/DetallePelicula/index.js
+++ proyecto-integrador/src/screens/DetallePelicula/index.js
@@ -30,24 +30,23 @@ class index extends Component {
               <div id='load'><h1 className='letter'>Trayendo película...</h1></div>
           </div>
         :
-           <section>
-            <article className='character-card'>
+           <section className='sectionDetalle'>
+            <div className='character-card'>
               <Link to={`/movie/id/${this.state.movieData.id}`}><img src= { 'https://image.tmdb.org/t/p/w500/'+this.state.movieData.poster_path} alt={this.state.movieData.title} className='image'/></Link>
-              <h3 className='letter'>{this.state.movieData.title}</h3> 
-            </article>
+            </div>
               <article className="cajahijapelis">
-              <h2 className="titulo" id="titulo2">TÍTULO: {this.state.movieData.title}</h2>
-              <p className="subtitulo" id="data">RATING: {this.state.movieData.vote_average}</p>
-              <p className="subtitulo" id="data">FECHA DE ESTRENO:  { this.state.movieData.release_date}</p>
-              <p className="subtitulo" id="data">GÉNERO: </p>
+              <h2 className="titulo" > {this.state.movieData.title}</h2>
+              <p className="subtitulo">RATING: {this.state.movieData.vote_average}</p>
+              <p className="subtitulo">FECHA DE ESTRENO:  { this.state.movieData.release_date}</p>
+              <p className="subtitulo">GÉNERO: </p>
               
-              <p className="subtitulo" id="data">DURACIÓN: {this.state.movieData.runtime} minutes</p>
-              <p className="subtitulo" id="data">SINOPSIS: {this.state.movieData.overview}</p>
+              <p className="subtitulo">DURACIÓN: {this.state.movieData.runtime} minutes</p>
+              <p className="subtitulo">SINOPSIS: {this.state.movieData.overview}</p>
               {
                 this.state.isFav ?
-                <button onClick={() => this.removeFav(this.state.movieData.id)}>REMOVE FROM FAV</button>
+                <button className='botonFav' onClick={() => this.removeFav(this.state.movieData.id)}>REMOVE FROM FAV</button>
                 :
-                <button onClick={() => this.addToFav(this.state.movieData.id)}>ADD TO FAV</button>
+                <button className='botonFav' onClick={() => this.addToFav(this.state.movieData.id)}>ADD TO FAV</button>
               }
                 
                 
--- proyecto-integrador/src/screens/DetalleSerie/index.js
+++ proyecto-integrador/src/screens/DetalleSerie/index.js
@@ -30,24 +30,23 @@ class index extends Component {
               <div id='load'><h1 className='letter'>Trayendo serie...</h1></div>
           </div>
         :
-           <section>
-            <article className='character-card'>
+           <section className='sectionDetalle'>
+            <div className='character-card'>
               <Link to={`/movie/id/${this.state.movieData.id}`}><img src= { 'https://image.tmdb.org/t/p/w500/'+this.state.movieData.poster_path} alt={this.state.movieData.title} className='image'/></Link>
-              <h3 className='letter'>{this.state.movieData.name}</h3> 
-            </article>
+            </div>
               <article className="cajahijapelis">
-              <h2 className="titulo" id="titulo2">{this.state.movieData.name}</h2>
-              <p className="subtitulo" id="data">RATING: {this.state.movieData.vote_average}</p>
-              <p className="subtitulo" id="data">FECHA DE ESTRENO:  { this.state.movieData.first_air_date}</p>
-              <p className="subtitulo" id="data">GÉNERO: { this.state.movieData.first_air_date}</p>
+              <h2 className="titulo">{this.state.movieData.name}</h2>
+              <p className="subtitulo">RATING: {this.state.movieData.vote_average}</p>
+              <p className="subtitulo">FECHA DE ESTRENO:  { this.state.movieData.first_air_date}</p>
+              <p className="subtitulo">GÉNERO: { this.state.movieData.first_air_date}</p>
              
-              <p className="subtitulo" id="data">DURACIÓN: {this.state.movieData.episode_run_time} minutes</p>
-              <p className="subtitulo" id="data">SINOPSIS: {this.state.movieData.overview}</p>
+              <p className="subtitulo">DURACIÓN: {this.state.movieData.episode_run_time} minutes</p>
+              <p className="subtitulo" id='sinopsis'>SINOPSIS: {this.state.movieData.overview}</p>
               {
                 this.state.isFav ?
-                <button onClick={() => this.removeFav(this.state.movieData.id)}>REMOVE FROM FAV</button>
+                <button className='botonFav' onClick={() => this.removeFav(this.state.movieData.id)}>REMOVE FROM FAV</button>
                 :
-                <button onClick={() => this.addToFav(this.state.movieData.id)}>ADD TO FAV</button>
+                <button className='botonFav' onClick={() => this.addToFav(this.state.movieData.id)}>ADD TO FAV</button>
               }
                 
                 
